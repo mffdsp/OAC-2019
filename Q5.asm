4e377098394cfa33bc7e7df9fe703cf8feb26a76
Mateus Felismino <45128866+mffdsp@users.noreply.github.com>
2019-10-22 21:08:59 -0300

    Update Q5.asm

--- Q5.asm
+++ Q5.asm
@@ -1,26 +1,26 @@
 BEGIN:
-#s0 corresponde ao a. $s1 corresponde ao b. 
-addi $s0, $zero, 1 #Adicionando valor no registrador $s00,
+
+addi $s0, $zero, 1 #Adicionando valor no registrador $s0
 addi $s1, $zero, -3 #Adicionando valor no registrador $s2
-addi $v0, $zero, 0 #Registrador auxiliar para guardar o sinal de $s0 (1 para negativo, 0 para positivo)
-addi $v1, $zero, 0 #Registrador auxiliar para guardar o sinal de $s1 (1 para negativo, 0 para positivo)
-slt $v0, $s0, $zero #Verificando se a<0 ($zero<$s0)
-slt $v1, $s1, $zero #Verifica se b<0
+addi $v0, $zero, 0 #Registrador auxiliar para sinal de $s0 (1 para negativo, 0 para positivo)
+addi $v1, $zero, 0 #Registrador auxiliar para sinal de $s1 (1 para negativo, 0 para positivo)
+slt $v0, $s0, $zero #Verifica se a < 0 
+slt $v1, $s1, $zero #Verifica se b < 0
 
-addi $t0, $zero, 0 #atribui valor da multiplicaÁ„o
+addi $t0, $zero, 0 #atribui valor da multiplica√ß√£o
 
-MULT: #Realiza a multiplicaÁ„o entre o par de numeros
+MULT: #Realiza a multiplica√ß√£o entre o par de numeros
 
 beq $s0, $zero, SINAL #Caso base, quando o $s0 chegar em 0.
-add $t0, $t0, $s1 #Somando o n˙mero $s1 ao registrador $t0
+add $t0, $t0, $s1 #Somando o n√∫mero $s1 ao registrador $t0
 
 j INCREMENTO 
 
-INCREMENTO: #Verifica que tipo de incremento ser· realizado
+INCREMENTO: #Verifica que tipo de incremento ser√° realizado
 
 slt $t1, $zero, $s0 #Se negativo, soma-se 1
 beq $t1, $zero, INCREMENTO_POSITIVO
-addi $s0, $s0, -1 #Se positivo, ser· subtraido 1
+addi $s0, $s0, -1 #Se positivo, ser√° subtraido 1
 	
 j MULT
 
@@ -38,18 +38,18 @@ j END
 
 POSITIVO: #Se o valor for > 0, ou seja positivo
 
-slt $t1, $zero, $t0 #VerificaÁ„o
-beq $t1, $zero, INVERTE_SINAL #Caso n„o seja positivo, inverte-se o sinal
+slt $t1, $zero, $t0 #Verifica√ß√£o
+beq $t1, $zero, INVERTE_SINAL #Caso n√£o seja positivo, inverte-se o sinal
 	
 j END
 	
-SINAL: #ApÛs realizar a multiplicaÁ„o , verificar· qual ser· o sinal do n˙mero
+SINAL: #Ap√≥s realizar a multiplica√ß√£o , verificar√° qual ser√° o sinal do n√∫mero
 
-beq $v0, $v1, POSITIVO #Se ambos os sinais forem iguais, pula para a label, se n„o segue-se.
+beq $v0, $v1, POSITIVO #Se ambos os sinais forem iguais, pula para a label, se n√£o segue-se.
 	
 NEGATIVO: 
 	
-slt $t1, $zero, $t0 #Verifica se $to j· È negativo.
+slt $t1, $zero, $t0 #Verifica se $to j√° √© negativo.
 bne $t1, $zero, INVERTE_SINAL #Caso seja positivo inverte-se o sinal.
 	
 END: # Valor final fixado em $t0
